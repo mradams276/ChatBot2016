8b829c510e4ef3c8d1bacb707981e07790cb5ddc
mradams276 <kyle.adams276@gmail.com>
2016-10-14 08:54:13 -0600

    Started Project

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,12 @@
+package chat.controller;
+
+public class ChatbotRunner
+{
+
+		public static void main (String [] args)
+		{
+			ChatController chatAppController = new ChatController();
+			chatAppController.start();
+		
+		}
+}
